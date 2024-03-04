300282de152d7eda5c26ea0846b92e0f50b86d5b
okosvagyok <kemenes.marton@blathy.info>
2024-03-04 14:30:29 +0100

    almanagyitas

--- callstack/main.js
+++ callstack/main.js
@@ -1,4 +1,5 @@
-const multiply = (x,y)=>x*y;
-const square = (x)=> multiply(x,x);
-const squareSum = (a,b) => square(a)+square(b);
-console.log(squareSum(2,3));
+document.addEventListener("DOMContentLoaded", betolt)
+
+function betolt() {
+    document.getElementById("apple").style.transform = "scale(1, 5)";
+}
